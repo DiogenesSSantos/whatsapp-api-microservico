b73ef776b8a5fa3a39302d9e8a04eb44f36655ed
Diogenes santos <diogenescontatoofficial@hotmail.com>
2026-02-22 20:47:00 -0300

    Melhorando a documentação do OPEN-API.

--- src/main/kotlin/com/git/devdioge/whatsapp_api/config/ConfigurationOpenApi.kt
+++ src/main/kotlin/com/git/devdioge/whatsapp_api/config/ConfigurationOpenApi.kt
@@ -1,6 +1,10 @@
 package com.git.devdioge.whatsapp_api.config
 
+import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,9 +17,44 @@ import org.springframework.context.annotation.Configuration
 class ConfigurationOpenApi {
 
     @Bean
-    fun configOpenApi() : OpenAPI {
-        return OpenAPI()
+    fun config(): OpenAPI? {
+        return OpenAPI().info(
+            Info().title("Api whatsapp continus-deployment CI 1.0.0")
+                .description(
+                    "Api-Rest whatsapp"
+                )
+                .version("1.0.0")
+                .contact(
+                    Contact().url("https://diogenesssantos.github.io/meu-portfolio/")
+                        .name("Diogenes S Santos").email("diogenescontatoofficial@hotmail.com")
+                )
+                .summary("envia mensagem para usuario utilizando outros serviço como N8N e Evolution-api")
+                .license(
+                    License()
+                        .name("MIT License")
+                        .url("https://opensource.org/licenses/MIT")
+                )
+        )
+            .externalDocs(
+                ExternalDocumentation()
+                    .description("GitHub")
+                    .url("https://github.com/DiogenesSSantos")
+            )
+            .externalDocs(
+                ExternalDocumentation()
+                    .description("Documentação JavaDoc")
+                    .url("http://devdiogenes.shop/apidocs/index.html")
+            )
     }
+
+    @Bean
+    fun configWhatsappController() : GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("WhatsappController")
+            .pathsToMatch("/whatsapp/**")
+            .build()
+    }
+
 
     @Bean
     fun configQrController() : GroupedOpenApi {
